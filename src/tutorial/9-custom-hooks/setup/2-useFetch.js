c7ed4ec801fbb60ba9a3fb6e3ba26f591bdd7df3
Fillipe Augusto <fillipe484@gmail.com>
2021-02-10 11:53:32 -0300

    useCallback fetch example

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,20 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export const useFetch = (url) => {
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
 
-  const getProducts = async () => {
+  const getProducts = useCallback(async () => {
     const response = await fetch(url);
     const products = await response.json();
     setProducts(products);
     setLoading(false);
-  };
+  }, [url]);
 
   useEffect(() => {
     getProducts();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [url]);
+  }, [getProducts, url]);
 
   return { loading, products };
 };
